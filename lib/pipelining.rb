9e6f0f291ab88ad9a86f529f925c5d9aec77c1d9
Karl Eklund <karl@magplus.com>
2013-08-26 23:14:57 +0200

    Cleanup

--- lib/pipelining.rb
+++ lib/pipelining.rb
@@ -1,23 +1,21 @@
 require "pipelining/version"
 
-#module Pipelining
-  def pipe object, &block
-    Class.new(BasicObject) do
-      attr_reader :state
+def pipe object, &block
+  Class.new(BasicObject) do
+    attr_reader :state
 
-      def self.run object, &block
-        runner = new object
-        runner.instance_eval(&block)
-      end
+    def self.run object, &block
+      runner = new object
+      runner.instance_eval(&block)
+    end
 
-      def initialize starting_object
-        @state = starting_object
-      end
+    def initialize starting_object
+      @state = starting_object
+    end
 
-      def method_missing(m, *args, &block)
-        @state = state.send(m, *args, &block)
-        self
-      end
-    end.run(object, &block).state
-  end
-#end
+    def method_missing(m, *args, &block)
+      @state = state.send(m, *args, &block)
+      self
+    end
+  end.run(object, &block).state
+end
